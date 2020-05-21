6f3f2516e353a43e26b182a68a9498f001bb1edb
fethi <abi_ayad.fethi_bey@courrier.uqam.ca>
2020-05-21 05:36:33 -0400

    modification fizzbuzz a 200

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -1,6 +1,6 @@
 public class Fizzbuzz {
     public static void main(String[] args) {
-        for (int i = 1 ; i<= 100 ; i++){
+        for (int i = 1 ; i<= 200 ; i++){
             if (i % 3 == 0 && i % 5 == 0)
                 System.out.println("fizzbuzz");
             else if (i % 5 == 0 )
