7e32aa55d96767295f1a39d55e0f562b148a39e9
fethi bey <fethibeyabiayad@gmail.com>
2020-05-21 01:26:33 -0400

    creationde la classe Fizzbuzz

---
+++ src/Fizzbuzz.java
@@ -0,0 +1,14 @@
+public class Fizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 1 ; i<= 100 ; i++){
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("fizzbuzz");
+            else if (i % 5 == 0 )
+                System.out.println("buzz");
+            else if (i % 3 == 0)
+                System.out.println("fizz");
+            else
+                System.out.println(i);
+        }
+    }
+}
